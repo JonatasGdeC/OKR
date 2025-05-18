b3eb3009ef28c6a566c8a8aeb6ed97ee93e3d5af
JonatasGdeC <jonatas.07022003@gmail.com>
2025-05-18 15:11:38 -0300

    Ampliando verificação em RegisterValidator

--- src/OKR.Application/UseCases/Objetives/Register/RegisterObjectiveValidator.cs
+++ src/OKR.Application/UseCases/Objetives/Register/RegisterObjectiveValidator.cs
@@ -7,8 +7,8 @@ public class RegisterObjectiveValidator : AbstractValidator<RequestObjectiveJson
 {
   public RegisterObjectiveValidator()
   {
-    RuleFor(objective => objective.Title).NotEmpty().WithName("Title is required.");
-    RuleFor(objective => objective.Year).NotEmpty().WithName("Year is required.");
-    RuleFor(objective => objective.Quarter).NotEmpty().WithName("Quarter is required.");
+    RuleFor(objective => objective.Title).NotEmpty().WithName("Title is required.").MinimumLength(3).WithMessage("Title must have at least 3 characters").MaximumLength(100).WithMessage("Title must have a maximum of 25 characters");
+    RuleFor(objective => objective.Year).NotEmpty().WithName("Year is required.").Must(year => year >= DateTime.Now.Year).WithMessage("Year must be the current year or later.");;
+    RuleFor(objective => objective.Quarter).NotEmpty().WithName("Quarter is required.").InclusiveBetween(1, 4).WithMessage("Quarter must be between 1 and 4.");;
   }
 }
