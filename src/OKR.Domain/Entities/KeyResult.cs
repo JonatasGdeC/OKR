539fcc6e55c9e079ddade8d1457161893f09550a
JonatasGdeC <jonatas.07022003@gmail.com>
2025-05-19 22:40:04 -0300

    Criando entidade KeyResult

---
+++ src/OKR.Domain/Entities/KeyResult.cs
@@ -0,0 +1,15 @@
+namespace OKR.Domain.Entities;
+
+public class KeyResult
+{
+  public Guid Id { get; init; }
+  public Guid ObjectiveId { get; init; }
+  public int NumberKr { get; init; }
+  public required string Title { get; init; }
+  public DateTime CreateDate { get; init; }
+
+  public KeyResult()
+  {
+    CreateDate = DateTime.UtcNow;
+  }
+}
